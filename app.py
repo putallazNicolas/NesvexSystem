e60ab59d7ec39ddb7d4ef6eed0c4b326f381d467
Nicolás Putallaz <77561348+putallazNicolas@users.noreply.github.com>
2025-01-13 16:29:08 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,144 @@
+import os
+import mysql.connector
+from dotenv import load_dotenv
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from functools import wraps
+from random import shuffle
+
+app = Flask(__name__)
+
+# Configuración de la base de datos
+# Cargar las variables desde el archivo .env
+load_dotenv()
+
+# Obtener las variables de entorno
+host = os.getenv("DB_HOST")
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+database = os.getenv("DB_NAME")
+app.secret_key = os.getenv("FLASK_SECRET_KEY")
+
+# Conectarse a la base de datos
+db = mysql.connector.connect(
+    host=host,
+    user=user,
+    password=password,
+    database=database
+)
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/latest/patterns/viewdecorators/
+    """
+
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+cursor = db.cursor()
+
+
+@app.route('/')
+@login_required
+def index():
+    return render_template("index.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+@login_required
+def register():
+    """Register user"""
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        passwordConfirmation = request.form.get("confirmation")
+
+        if not username:
+            return render_template("register.html", alert=True, alertMsg="Por favor introduce un usuario")
+        if not password:
+            return render_template("register.html", alert=True, alertMsg="Por favor introduce una contraseña")
+        if not passwordConfirmation:
+            return render_template("register.html", alert=True, alertMsg="Please confirm your password")
+
+        cursor.execute("SELECT COUNT(*) FROM usuarios WHERE username = %s", (username,))
+        (sameUsernameCount,) = cursor.fetchone()
+
+        if sameUsernameCount != 0:
+            return render_template("register.html", alert=True, alertMsg="El usuario ya existe")
+
+        if password != passwordConfirmation:
+            return render_template("register.html", alert=True, alertMsg="Las contraseñas no coinciden")
+
+        hash = generate_password_hash(password)
+
+        cursor.execute("INSERT INTO usuarios (username, hash) VALUES (%s, %s)", (username, hash))
+        db.commit()
+
+        return redirect("/")
+
+    else:
+        return render_template("register.html")
+    
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not username:
+            return render_template("login.html", alert=True, alertMsg = "Introduce el usuario")
+
+        # Ensure password was submitted
+        elif not password:
+            return render_template("login.html", alert=True, alertMsg = "Introduce la contraseña")
+
+        # Query database for username
+        query = "SELECT id, username, hash FROM usuarios WHERE username = %s"
+        cursor.execute(query, (username,))
+        result = cursor.fetchone()
+
+        if result:
+            storedId, storedUser, storedHash = result
+
+            # Ensure username exists and password is correct
+            if not check_password_hash(storedHash, password):
+                return render_template("login.html", alert=True, alertMsg = "Invalid username and/or password")
+
+        # Remember which user has logged in
+        session["user_id"] = storedId
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
